760cda519c6bdc0ba397a2a56ea118edd4ec09e1
Diego Go <diego.gomes87@gmail.com>
2026-03-16 07:51:52 +0100

    add urls for loans

--- backend/apps/loans/urls.py
+++ backend/apps/loans/urls.py
@@ -1,3 +1,8 @@
 from apps.loans.views import LoanApplicationViewSet
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r"applications", LoanApplicationViewSet, basename="loanapplication")
+
+urlpatterns = [path("", include(router.urls))]
