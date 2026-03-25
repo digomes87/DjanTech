4d3cac0dbbb61a153f0c1209e82f43762770673d
Diego Go <diego.gomes87@gmail.com>
2026-03-25 09:23:49 +0100

    add news views in uls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -17,4 +17,11 @@ urlpatterns = [
     path(API_V1 + "properties/", include("apps.properties.urls")),
     path(API_V1 + "notifications/", include("apps.notifications.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerSplitView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path("health/", include("core.urls")),
 ]
